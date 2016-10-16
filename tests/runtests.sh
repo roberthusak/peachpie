c1a020bbe3a89eb5da2b905378a68a216bd2dddd
Robert Husák <robert@husak.cloud>
2016-10-16 11:52:22 +0200

    Dump src folder for Travis CI debugging

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,7 +2,8 @@ php ./tests/arrays/foreach_001.php
 
 dotnet restore ./Samples/ConsoleApplication1
 
-# Diagnose the presence of particular nuget outputs in the filesystem
+# Diagnose the presence of various compilation and packaging outputs in the filesystem
+find ./src
 find ~/.nuget/packages/.tools
 find ~/.nuget/packages/Peachpie.Compiler.Tools
 cat  ~/.nuget/packages/Peachpie.Compiler.Tools/0.2.0-beta/lib/netcoreapp1.0/dotnet-compile-php.runtimeconfig.json
