d7c8cdae80f9726b3b1d05c7e1f69c97793a6f99
Robert Husák <robert@husak.cloud>
2016-10-16 13:04:25 +0200

    Revert "Add nuget debugging outputs for dotnet-compile-php"
    This reverts commit 7a225c9ebe79dcf8d1a7959e4ada145d69fc9aef.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,12 +1,6 @@
 php ./tests/arrays/foreach_001.php
 
 dotnet restore ./Samples/ConsoleApplication1
-
-# Diagnose the presence of particular nuget outputs in the filesystem
-find ~/.nuget/packages/.tools
-find ~/.nuget/packages/Peachpie.Compiler.Tools
-cat  ~/.nuget/packages/Peachpie.Compiler.Tools/0.2.0-beta/lib/netcoreapp1.0/dotnet-compile-php.runtimeconfig.json
-
 dotnet build -f .netcoreapp1.0 ./Samples/ConsoleApplication1
 
 cd ./Samples/ConsoleApplication1
