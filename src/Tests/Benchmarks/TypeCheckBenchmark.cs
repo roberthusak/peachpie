08887d19f3e97aef4295651f23a364684489cfb9
Robert Husák <robert@husak.cloud>
2021-05-07 15:58:42 +0200

    Add benchmark of dynamic type checking

---
+++ src/Tests/Benchmarks/TypeCheckBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    public class TypeCheckBenchmark
+    {
+        class C
+        {
+            [MethodImpl(MethodImplOptions.NoInlining)]
+            public void O()
+            {
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void SimpleIsCallee(object o)
+        {
+            if (o is C)
+            {
+                ((C)o).O();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void IsPatternMatchCallee(object o)
+        {
+            if (o is C c)
+            {
+                c.O();
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void SimpleIs() => SimpleIsCallee(new C());
+
+        [Benchmark]
+        public void IsPatternMatch() => IsPatternMatchCallee(new C());
+    }
+}
